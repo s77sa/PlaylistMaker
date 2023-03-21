f0b74562939443ba8dc94b7f40a01964b5d4264b
s77sa <s77sa@yandex.ru>
2023-03-21 17:21:58 +0300

    Коммит №1 перед сдачей задания

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,48 +1,59 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.example.playlistmaker.utils.Intents
 
+@Suppress("DUPLICATE_LABEL_IN_WHEN")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.println(Log.INFO, "my_tag", "onCreate_Main")
         onClickListeners() // Запуск всех прослушивателей на активити
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.println(Log.INFO, "my_tag", "onSave_Main")
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.println(Log.INFO, "my_tag", "onRestore_Main")
+    }
     private fun onClickListeners(){
         onClickSearch()
         onClickLibrary()
         onClickSettings()
+
     }
 
     private fun onClickSearch(){
         val item = findViewById<Button>(R.id.btn_search)
-        item.setOnClickListener(clickListener)
+        item.setOnClickListener(clickListener())
     }
     private fun onClickLibrary(){
         val item = findViewById<Button>(R.id.btn_library)
-        item.setOnClickListener(clickListener)
+        item.setOnClickListener(clickListener())
     }
 
     private fun onClickSettings(){
         val item = findViewById<Button>(R.id.btn_settings)
-        item.setOnClickListener(clickListener)
+        item.setOnClickListener(clickListener())
     }
 
-    // Подсмотрел подобную реализацию на stackoverflow
-    private val clickListener = View.OnClickListener { view ->
+    private fun clickListener() = View.OnClickListener { view ->
+        val intents = Intents()
         when (view.id) {
-            R.id.btn_settings -> intentCall(SettingsActivity::class.java)
-            R.id.btn_library -> intentCall(LibraryActivity::class.java)
-            R.id.btn_search -> intentCall(SearchActivity::class.java)
+            R.id.btn_settings -> intents.intentCall(this, SettingsActivity::class.java)
+            R.id.btn_library -> intents.intentCall(this, LibraryActivity::class.java)
+            R.id.btn_search -> intents.intentCall(this, SearchActivity::class.java)
         }
-    }
-    private fun intentCall(activityClass: Class<out AppCompatActivity>){
-        val intent = Intent(this, activityClass)
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import com.example.playlistmaker.utils.Intents
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -15,12 +17,38 @@ class SettingsActivity : AppCompatActivity() {
 
     private fun onClickListeners(){
         onClickReturn()
+        onClickShare()
+        onClickSupport()
+        onClickTerms()
     }
 
-    private fun onClickReturn(){
+    private fun onClickTerms(){ // Соглашение
+        val item = findViewById<TextView>(R.id.tv_terms)
+    item.setOnClickListener(clickListener())
+    }
+
+    private fun onClickSupport(){ // Поддержка
+        val item = findViewById<TextView>(R.id.tv_support)
+        item.setOnClickListener(clickListener())
+    }
+
+    private fun onClickShare(){ // Поделиться
+        val item = findViewById<TextView>(R.id.tv_share)
+        item.setOnClickListener(clickListener())
+    }
+
+    private fun onClickReturn(){ // Возврат на предыдущий экран
         val item = findViewById<ImageView>(R.id.iv_settings_back)
-        item.setOnClickListener{
-            this.finish()
+        item.setOnClickListener(clickListener())
+    }
+
+    private fun clickListener() = View.OnClickListener { view ->
+        val intents = Intents()
+        when (view.id) {
+            R.id.iv_settings_back -> this.finish()
+            R.id.tv_share -> intents.intentSend(this, getString(R.string.link_practicum_ad))
+            R.id.tv_support -> intents.intentEmail(this, getString(R.string.email_dest), getString(R.string.email_text))
+            R.id.tv_terms -> intents.intentOpenLink(this, getString(R.string.link_terms))
         }
     }
 }
