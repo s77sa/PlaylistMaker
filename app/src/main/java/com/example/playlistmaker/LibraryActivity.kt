496568d8c43d870ca8e7cca318a975e4037b2578
s77sa <s77sa@yandex.ru>
2023-03-22 22:58:30 +0300

    Коммит №2. Так-как небыло никаких комментариев, то что-то поправил :)

--- app/src/main/java/com/example/playlistmaker/LibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/LibraryActivity.kt
@@ -2,10 +2,29 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.example.playlistmaker.utils.Intents
 
 class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
+        onClickListeners() // Запуск всех прослушивателей на активити
+    }
+
+    private fun onClickListeners(){
+        onClickReturn()
+    }
+    private fun onClickReturn(){ // Возврат на предыдущий экран
+        val item = findViewById<ImageView>(R.id.iv_library_back)
+        item.setOnClickListener(clickListener())
+    }
+
+    private fun clickListener() = View.OnClickListener { view ->
+        //val intents = Intents()
+        when (view.id) {
+            R.id.iv_library_back -> this.finish()
+        }
     }
 }
