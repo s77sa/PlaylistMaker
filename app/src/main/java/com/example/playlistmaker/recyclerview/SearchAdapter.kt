7d970f7ddb53b752218c17c2d422ca729579392c
s77sa <s77sa@yandex.ru>
2023-06-07 23:33:37 +0300

    Добавил отслеживание времени проигрывания

--- app/src/main/java/com/example/playlistmaker/recyclerview/SearchAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/recyclerview/SearchAdapter.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker.recyclerview
 
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
